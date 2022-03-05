02efd7b0fe599f88915d463cb1c1dc1c78ed1ca9
Nguyen <giaanguyen221197@gmail.com>
2022-03-05 21:07:41 +0700

    Fix Video Rendering In Mobile v4

--- src/modules/landing-page/components/Main/index.tsx
+++ src/modules/landing-page/components/Main/index.tsx
@@ -1,31 +1,30 @@
-import { useEffect, useRef } from 'react';
+import { useRef } from 'react';
 import './style.scss';
 import './responsive.scss';
 import Hero from '../../../../assets/Hero.mp4';
 import useOnScreen from '../../../../hooks/UseOnScreen';
+import ReactPlayer from 'react-player';
 
 const Main = () => {
   const videoRef = useRef<any>(null);
   const onScreen = useOnScreen(videoRef, '-300px', 0.1, false);
-
-  useEffect(() => {
-    videoRef.current.defaultMuted = true;
-  });
-
-  if (videoRef.current) {
-    if (onScreen) {
-      videoRef.current.play();
-    } else {
-      videoRef.current.pause();
-    }
-  }
-
+  
   return (
-    <div className="main">
-      <video ref={videoRef} loop autoPlay muted playsInline>
+    <div className="main" ref={videoRef}>
+      <ReactPlayer
+        playing={onScreen}
+        url={Hero}
+        muted
+        loop
+        playsinline
+        height="100%"
+        width="100%"
+      />
+
+      {/* <video ref={videoRef} loop autoPlay muted playsInline>
         <source src={Hero} type="video/mp4" />
         Your browser does not support the video tag.
-      </video>
+      </video> */}
     </div>
   );
 };
