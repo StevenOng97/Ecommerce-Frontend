fd8f41038424258d491d3138f5a7cc9fe5d8ee22
Dust07 <tienphat0707@gmail.com>
2022-04-02 22:35:43 +0700

    Navbar highlight current page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Route, Routes, useLocation } from 'react-router-dom';
-import { AnimatePresence } from 'framer-motion';
 import Loader from './components/Loader';
 import './main.scss';
 import 'react-phone-input-2/lib/style.css';
@@ -14,7 +13,10 @@ import Modal from './components/Modal';
 import { handleModalState } from './redux/actions/modal';
 import Layout from './modules/main-layout';
 import ErrorPage from './modules/error-page';
-
+import { AnimatePresence } from 'framer-motion';
+import Header from './modules/landing-page/components/Header';
+import Footer from './modules/landing-page/components/Footer';
+import AnimatedPage from './components/AnimatedPage';
 function App() {
   const loading = useSelector((state: any) => state.products.isLoading);
   const showModal = useSelector((state: any) => state.modal.isOpen);
@@ -40,17 +42,37 @@ function App() {
   return (
     <div className={className}>
       <Loader loading={loading} />
-      <AnimatePresence exitBeforeEnter>
-        <Layout>
+      <Header cartCount={2} />
+      <AnimatePresence initial={false} exitBeforeEnter>
         <Routes key={location.pathname} location={location}>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="register" element={<RegisterPage />} />
-          <Route path="login" element={<LoginPage />} />
-          <Route path="products/:id" element={<Product />} />
-          <Route path="*" element={<ErrorPage/>}/>
+          <Route path="/" element={
+            <AnimatedPage>
+              <LandingPage />
+            </AnimatedPage>
+          } />
+          <Route path="register" element={
+            <AnimatedPage>
+              <RegisterPage />
+            </AnimatedPage>
+          } />
+          <Route path="login" element={
+            <AnimatedPage>
+              <LoginPage />
+            </AnimatedPage>
+          } />
+          <Route path="products/:id" element={
+            <AnimatedPage>
+              <Product />
+            </AnimatedPage>
+          } />
+          <Route path="*" element={
+            <AnimatedPage>
+              <ErrorPage />
+            </AnimatedPage>
+          } />
         </Routes>
-        </Layout>
       </AnimatePresence>
+      <Footer />
       <Modal showModal={showModal} closeBtnAction={modalActionBtn} />
     </div>
   );
